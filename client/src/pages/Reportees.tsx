6c58b8158dc10eda484fa3d90b41a64081287bb0
rbaronia <37376296+rbaronia@users.noreply.github.com>
2025-01-30 10:19:26 +0800

    fix: correct import paths for api utility

--- client/src/pages/Reportees.tsx
+++ client/src/pages/Reportees.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import api from '../../utils/api';
+import api from '../utils/api';
 import { Paper, Typography, List, ListItem, ListItemText } from '@mui/material';
 
 const Reportees: React.FC = () => {
